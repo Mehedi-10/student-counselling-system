964afa9d7211ac8959a3906850ea77c7978e2e87
mehedi <mehedihasanarafat10@gmail.com>
2024-05-08 08:37:28 +0600

    added jwt and swagger

---
+++ scs/serializers.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.hashers import make_password
+from rest_framework import serializers
+from .models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password', 'is_student', 'is_teacher']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def create(self, validated_data):
+        user = User(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            is_student=validated_data.get('is_student', False),
+            is_teacher=validated_data.get('is_teacher', False)
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
